6390ad81e34560a6607c0490cf0ffbcbf191e310
Harold Bradley III <hbradleyiii@bradleystudio.net>
2019-08-16 07:08:27 -0400

    Simple refactoring to make the code a bit more readable.

--- script.js
+++ script.js
@@ -26,128 +26,121 @@
 
 (function () {
 
-var w = null;
-var kDoc = null;
-var kObj = null;
+    if (typeof window !== 'object') {
+        alert('Error: Kindle Optimizer is not active. The Amazon Cloud Reader window could not be found.');
+        return;
+    }
 
+    KindleReaderContextMenu.ACRExtensions = true;
 
-w = window;
+    var original_show = KindleReaderContextMenu.show;
 
-if (typeof w === 'object') {
-    kObj = w.KindleReaderContextMenu;
-    kDoc = w.document;
-        kObj.ACRExtensions = true;
-        var oldMethod = kObj.show;
-        kObj.show = function () {
-            var res = oldMethod.apply(kObj, arguments);
-            var txtDoc = null;
-            var r = null;
+    KindleReaderContextMenu.show = function () {
+        var txtDoc = null;
+        var selected_text_range = null;
 
-            if (typeof (arguments[3]) !== 'undefined' && typeof (arguments[3]['start']) !== 'undefined') {
-                var sId = arguments[3]['start'];
-                var eId = arguments[3]['end'];
+        if (typeof (arguments[3]) !== 'undefined' && typeof (arguments[3]['start']) !== 'undefined') {
+            var sId = arguments[3]['start'];
+            var eId = arguments[3]['end'];
 
-                $('iframe', kDoc).each(function (j, textIframe) {
-                    var textIFrameDoc = $(textIframe).contents().get(0);
-                    if ($('#'+sId, textIFrameDoc).get(0)) {
-                        txtDoc = textIFrameDoc;
-                        return false;
-                    }
-                });
-
-                if (txtDoc) {
-                    r = txtDoc.createRange();
-                    r.setStartBefore($('#'+sId, txtDoc).get(0));
-                    r.setEndAfter($('#'+eId, txtDoc).get(0));
+            $('iframe', document).each(function (j, textIframe) {
+                var textIFrameDoc = $(textIframe).contents().get(0);
+                if ($('#'+sId, textIFrameDoc).get(0)) {
+                    txtDoc = textIFrameDoc;
+                    return false;
                 }
+            });
+
+            if (txtDoc) {
+                selected_text_range = txtDoc.createRange();
+                selected_text_range.setStartBefore($('#'+sId, txtDoc).get(0));
+                selected_text_range.setEndAfter($('#'+eId, txtDoc).get(0));
             }
+        }
 
-            $('#ACRExtensions_searchB_sep', kDoc).remove();
-            $('#ACRExtensions_searchB', kDoc).remove();
-            var sepEl = $('<div id="ACRExtensions_searchB_sep" class="kindle_menu_separator"></div>');
-            var searchB = $('<div id="ACRExtensions_searchB" class="kindle_menu_button button_enabled ui-corner-left">Search</div>');
-            $('#kindle_menu_border', kDoc).append(sepEl).append(searchB);
-            setTimeout(function(){
-                sepEl.show();
-                searchB.removeClass('button_hidden');
-            }, 1);
-            $('#ACRExtensions_searchB', kDoc).click(function (evt) {
-                if (r) {
+        $('#ACRExtensions_searchB_sep', document).remove();
+        $('#ACRExtensions_searchB', document).remove();
+        var sepEl = $('<div id="ACRExtensions_searchB_sep" class="kindle_menu_separator"></div>');
+        var searchB = $('<div id="ACRExtensions_searchB" class="kindle_menu_button button_enabled ui-corner-left">Search</div>');
+        $('#kindle_menu_border', document).append(sepEl).append(searchB);
+        setTimeout(function(){
+            sepEl.show();
+            searchB.removeClass('button_hidden');
+        }, 1);
+        $('#ACRExtensions_searchB', document).click(function (evt) {
+            if (selected_text_range) {
 
-                    var content = r.cloneContents();
-                    var text = content.textContent;
-                    var url = 'https://www.google.com/?gfe_rd=cr&ei=ogkeWeD0NvTJXpeog4gK&gws_rd=cr&fg=1#q=';
-                    var newTab = window.open(url + text, '_blank');
-                    newTab.focus();
-                }
-            });
+                var content = selected_text_range.cloneContents();
+                var text = content.textContent;
+                var url = 'https://www.google.com/?gfe_rd=cr&ei=ogkeWeD0NvTJXpeog4gK&gws_rd=cr&fg=1#q=';
+                var newTab = window.open(url + text, '_blank');
+                newTab.focus();
+            }
+        });
 
-            $('#ACRExtensions_translateB_sep', kDoc).remove();
-            $('#ACRExtensions_translateB', kDoc).remove();
-            var sepEl = $('<div id="ACRExtensions_translateB_sep" class="kindle_menu_separator"></div>');
-            var translateB = $('<div id="ACRExtensions_translateB" class="kindle_menu_button button_enabled ui-corner-left">Translate</div>');
-            $('#kindle_menu_border', kDoc).append(sepEl).append(translateB);
-            setTimeout(function(){
-                sepEl.show();
-                translateB.removeClass('button_hidden');
-            }, 1);
-            $('#ACRExtensions_translateB', kDoc).click(function (evt) {
-                if (r) {
+        $('#ACRExtensions_translateB_sep', document).remove();
+        $('#ACRExtensions_translateB', document).remove();
+        var sepEl = $('<div id="ACRExtensions_translateB_sep" class="kindle_menu_separator"></div>');
+        var translateB = $('<div id="ACRExtensions_translateB" class="kindle_menu_button button_enabled ui-corner-left">Translate</div>');
+        $('#kindle_menu_border', document).append(sepEl).append(translateB);
+        setTimeout(function(){
+            sepEl.show();
+            translateB.removeClass('button_hidden');
+        }, 1);
+        $('#ACRExtensions_translateB', document).click(function (evt) {
+            if (selected_text_range) {
 
-                    var content = r.cloneContents();
-                    var text = content.textContent;
+                var content = selected_text_range.cloneContents();
+                var text = content.textContent;
 
-                    // This function is not used since 'googtrans' coockie is unavailable for kindle page.
-                    function readCookie(name) {
-                        var c = kDoc.cookie.split('; '),
-                        cookies = {}, i, C;
-                        for (i = c.length - 1; i >= 0; i--) {
-                            C = c[i].split('=');
-                            cookies[C[0]] = C[1];
-                        }
-                        return cookies[name];
+                // This function is not used since 'googtrans' coockie is unavailable for kindle page.
+                function readCookie(name) {
+                    var c = document.cookie.split('; '),
+                    cookies = {}, i, C;
+                    for (i = c.length - 1; i >= 0; i--) {
+                        C = c[i].split('=');
+                        cookies[C[0]] = C[1];
                     }
-
-                    function getBrowserLanguage() {
-                        var userLang = navigator.language || navigator.userLanguage;
-                        userLang = userLang.split('-')[0];
-                        return userLang;
-                    }
-
-                    var currentLanguage = getBrowserLanguage();
-
-                    if(typeof currentLanguage === 'undefined'){
-                        currentLanguage = 'en';
-                    } else if(currentLanguage === 'he'){
-                        currentLanguage = 'iw';
-                    } else if(currentLanguage === 'zh'){
-                        currentLanguage = 'zh-CN';
-                    }
-                    var url = 'https://translate.google.com/#auto/' + currentLanguage + '/';
-                    var newTab = window.open(url + text, '_blank');
-                    newTab.focus();
+                    return cookies[name];
                 }
-            });
 
-        $('#ACRExtensions_copyB_sep', kDoc).remove();
-            $('#ACRExtensions_copyB', kDoc).remove();
-            var sepEl = $('<div id="ACRExtensions_copyB_sep" class="kindle_menu_separator"></div>');
-            var copyB = $('<div id="ACRExtensions_copyB" class="kindle_menu_button button_enabled ui-corner-left">Copy</div>');
-            $('#kindle_menu_border', kDoc).append(sepEl).append(copyB);
-            setTimeout(function(){
-                sepEl.show();
-                copyB.removeClass('button_hidden');
-            }, 1);
-            $('#ACRExtensions_copyB', kDoc).click(function (evt) {
-                if (r) {
-                    var newW = window.open('', 'ACRExtensions', 'height=400,width=400,location=0,menubar=0,scrollbars=1,toolbar=0');
-                    newW.document.body.appendChild(r.cloneContents());
+                function getBrowserLanguage() {
+                    var userLang = navigator.language || navigator.userLanguage;
+                    userLang = userLang.split('-')[0];
+                    return userLang;
                 }
-            });
 
-            return res;
-        };
-} else {
-    alert('Error: Kindle Optimizer is not active. The Amazon Cloud Reader window could not be found.');
-}
+                var currentLanguage = getBrowserLanguage();
+
+                if(typeof currentLanguage === 'undefined'){
+                    currentLanguage = 'en';
+                } else if(currentLanguage === 'he'){
+                    currentLanguage = 'iw';
+                } else if(currentLanguage === 'zh'){
+                    currentLanguage = 'zh-CN';
+                }
+                var url = 'https://translate.google.com/#auto/' + currentLanguage + '/';
+                var newTab = window.open(url + text, '_blank');
+                newTab.focus();
+            }
+        });
+
+        $('#ACRExtensions_copyB_sep', document).remove();
+        $('#ACRExtensions_copyB', document).remove();
+        var sepEl = $('<div id="ACRExtensions_copyB_sep" class="kindle_menu_separator"></div>');
+        var copyB = $('<div id="ACRExtensions_copyB" class="kindle_menu_button button_enabled ui-corner-left">Copy</div>');
+        $('#kindle_menu_border', document).append(sepEl).append(copyB);
+        setTimeout(function(){
+            sepEl.show();
+            copyB.removeClass('button_hidden');
+        }, 1);
+        $('#ACRExtensions_copyB', document).click(function (evt) {
+            if (selected_text_range) {
+                var newW = window.open('', 'ACRExtensions', 'height=400,width=400,location=0,menubar=0,scrollbars=1,toolbar=0');
+                newW.document.body.appendChild(selected_text_range.cloneContents());
+            }
+        });
+
+        return original_show.apply(KindleReaderContextMenu, arguments);
+    };
 })();
